a0e697460c66c7df22ad837c7f575a6c94f14730
Andrey Drozd <drozdandrew58@gmail.com>
2021-11-25 15:33:47 +0300

    feat:added functionality for the menu-bg

---
+++ scripts/menu-bg.js
@@ -0,0 +1,32 @@
+const headerList = document.querySelector(".header__list");
+const logoMenu = document.querySelector(".logo-image__menu-bg");
+const menuBg = document.querySelector(".menu-bg");
+const main = document.querySelector(".main");
+
+const applyTransform = () => {
+    menuBg.classList.toggle("active")
+}
+
+const addShadowForWrapper = () => {
+    main.classList.toggle("shadow")
+}
+
+const showMenu = () => {
+    headerList.classList.toggle("show");
+}
+const showLogo = () => {
+    
+    if(headerList.classList.contains('show')) {
+        headerList.style.display = 'block';
+    } 
+    else {
+        headerList.style.display = 'none';
+    }
+}   
+
+menuBg.onclick = () => {
+    showMenu()
+    showLogo()
+    applyTransform()
+    addShadowForWrapper()
+};
