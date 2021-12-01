b192f5a87b3ae8c4139da9672275400c21545e21
Andrey Drozd <drozdandrew58@gmail.com>
2021-12-01 17:13:26 +0300

    refactor: change varaibles and add new constants

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,15 +1,16 @@
 const filterBox = document.querySelectorAll('.section-portfolio__image');
-document.querySelector('.section-portfolio__nav-list').addEventListener('click', event => {
+const buttonList = document.querySelector('.section-portfolio__nav-list');
+const handleClick = buttonList.addEventListener('click', event => {
     if(event.target.tagName !=='BUTTON') {
         return false;
     }
-    let filterClass = event.target.dataset['filter'];
+    const filterClass = event.target.dataset['filter'];
     
-    filterBox.forEach(elem => {
-        if(!elem.classList.contains(filterClass) && filterClass!=='all') {
-            elem.classList.add('hide')
+    filterBox.forEach(image => {
+        if(!image.classList.contains(filterClass) && filterClass!=='all') {
+            image.classList.add('hide')
         } else {
-            elem.classList.remove('hide');
+            image.classList.remove('hide');
         }
     });  
 
